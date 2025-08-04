f68879d2b3b71deb3d8253dd58d72cedd6810123
shivashankarpala <shivashankarpala7@gmail.com>
2025-08-04 17:50:34 +0530

    triangulator -initial

--- basic/triangle.py
+++ basic/triangle.py
@@ -1,38 +1,90 @@
 # Finds the element in a list with the largest length
-def lengthiest(lst): 
+def lengthiest(list): 
 
-    largest = len(str(lst[0]))
+    largest = len(str(list[0]))
     k = 0
-    for i in range(len(lst)):
-        if len(str(lst[i])) > largest:
-            largest = len(str(lst[i]))
+    for i in range(len(list)):
+        if len(str(list[i])) > largest:
+            largest = len(str(list[i]))
             k = i
     return k, largest
     # returns the index of the largest element and its length
 
-n = 20
-list1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+def print_equilateral_triangle(n, list, style):
 
-# Limit the n to the length of the list
-if n > len(list1):
-    n = len(list1)
+    # Limit the n to the length of the list
+    if n > len(list):
+        n = len(list)
 
 
-l, largest = lengthiest(list1)
-universal_space = len(str(list1[l])) 
-# Universal space is the length of the longest element in the list
+    l, largest = lengthiest(list)
+    universal_space = len(str(list[l])) 
+    # Universal space is the length of the longest element in the list
 
-for i in range(n):
-    print(' ' * (n - 1 - i) * universal_space, end='')
-    # Prints leading spaces for alignment
+    if style == 1:
+        for i in range(n):
+            print(' ' * (n - 1 - i) * universal_space, end='')
+            # Prints leading spaces for alignment
 
-    for j in range(i + 1):
+            for j in range(i + 1):
 
-        # If the element is less than the universal space, it will be padded with spaces
-        # All elements will have the same length when printed
-        if (len(str(list1[i])) < universal_space):
-            print(str(list1[i]) + ' ' * (universal_space - 1), end=' ' * universal_space)
-        else:
-            print(list1[i], end=' ' * universal_space)
-            
-    print() # New line after each row of the triangle
+                # If the element is less than the universal space, it will be padded with spaces
+                # All elements will have the same length when printed
+                if (len(str(list[i])) < universal_space):
+                    print(str(list[i]) + ' ' * (universal_space - 1), end=' ' * universal_space)
+                else:
+                    print(list[i], end=' ' * universal_space)
+                    
+            print() # New line after each row of the triangle
+
+    elif style == 2:
+        for i in range(n):
+            for j in range(n - i - 1):
+                print(' ', end=' ')
+            for j in range(i + 1):
+                print(str(list[j])  + ' ' * (universal_space - len(str(list[j]))), end=' ')
+            print()
+
+def print_right_triangle(n, list, side):
+
+    # Limit the n to the length of the list
+    if n > len(list):
+        n = len(list)
+
+    l, largest = lengthiest(list)
+    universal_space = len(str(list[l]))
+
+    if side == 1:
+        
+        # Right-angled triangle with the right angle at the top left
+        for i in range(n):
+            for j in range(n - i):
+                print(list[j], end=' ' * universal_space)
+            print()
+        
+
+    elif side == 2:
+
+        # Right-angled triangle with the right angle at the bottom left
+        for i in range(n):
+            for j in range(i + 1):
+                print(list[j], end=' ' * universal_space) 
+            print()
+    
+    elif side == 3:
+
+        # Right-angled triangle with the right angle at the top right
+        for i in range(n):
+            print(' ' * (n - 1 - i) * (universal_space + 1), end='')
+            for j in range(i + 1):
+                print(str(list[j]) + ' ' * (universal_space + 1 - len(str(list[j]))), end='')
+            print()
+
+    elif side == 4:
+
+        # Right-angled triangle with the right angle at the bottom right
+        for i in range(n):
+            print(' ' * i * (universal_space + 1), end='')
+            for j in range(n - i):
+                print(str(list[j]) + ' ' * (universal_space + 1 - len(str(list[j]))), end='')
+            print()
---
+++ basic/triangulator.py
@@ -0,0 +1,41 @@
+import triangle
+
+print("------------------------------Triangulator------------------------------")
+
+n = int(input("Number of Rows: "))
+list = [1, 200, 3, 400, 5, 600, 7, 800, 9, 100]
+
+print()
+print("Select Triangle: ")
+print("1. Equilateral Triangle")
+print("2. Right-Angled Triangle")
+
+choice = int(input("Enter your choice (1 or 2): "))
+print()
+
+
+if choice == 1:
+
+    print("Select Style of Equilateral Triangle: ")
+    print("1. Each row each element")
+    print("2. Each row with all elements")
+
+    type = int(input("Enter your choice (1 or 2): "))
+    print()
+
+    triangle.print_equilateral_triangle(n, list, type)
+
+elif choice == 2:
+
+    print("Select Right-Angled Triangle Orientation: ")
+    print("Right Angle at: ")
+    print("1. Top Left")
+    print("2. Bottom Left")
+    print("3. Top Right")
+    print("4. Bottom Right")
+    side = int(input("Enter your choice (1-4): "))
+    print()
+
+    triangle.print_right_triangle(n, list, side)
+
+print("------------------------------------------------------------------------")
